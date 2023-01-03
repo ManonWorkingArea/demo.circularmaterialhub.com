702670b7e4783276d3f52f0997112e7dff98d0c7
Manon Sanoi <info@manonsanoi.com>
2023-01-04 02:25:10 +0700

    DEV

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(isset($_GET["dev"]))
 else 
 {
     ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1;
+    ini_set('display_startup_errors', 1);
     error_reporting(E_ALL & ~E_NOTICE);
 }
 
