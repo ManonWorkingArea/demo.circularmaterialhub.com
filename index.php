3784cfd5ecfdfd4e04302b3e5e7f668a631f6b8b
Manon Sanoi <info@manonsanoi.com>
2023-01-04 03:35:33 +0700

    Re Server

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ if(isset($_GET["dev"]))
 }
 else 
 {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
     error_reporting(E_ALL & ~E_NOTICE);
 }
 
