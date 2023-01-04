d0933bae4277ed46e666efc12b1cea76d14f32fa
Manon Sanoi <info@manonsanoi.com>
2023-01-04 14:51:50 +0700

    Header

--- theme/skin/ecova/view_menu.php
+++ theme/skin/ecova/view_menu.php
@@ -1,11 +1,11 @@
 <!--Header Start-->
 <header class="header-style-2">
    <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand" href="home"><img src="theme/skin/ecova/assets/images/h2logo.png" alt=""></a>
+      <a class="navbar-brand" href="home">CMH LOGO</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
          <ul class="navbar-nav mr-auto">
-           <li class="nav-item"> <a class="nav-link" href="page/view/guidelines">หลักเกณฑ์ Eco Factory</a> </li>
+           <li class="nav-item"> <a class="nav-link" href="page/view/guidelines">หลักเกณฑ์ CMH</a> </li>
            <li class="nav-item"> <a class="nav-link" href="page/view/benefits">สิทธิประโยชน์</a> </li>
            <li class="nav-item"> <a class="nav-link" href="page/view/procedure">ขั้นตอนและตารางประชุม</a> </li>
            <li class="nav-item"> <a class="nav-link" href="page/view/news">ประชาสัมพันธ์</a> </li>
